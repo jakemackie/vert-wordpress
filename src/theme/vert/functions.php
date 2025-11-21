5e50aecd9a5f6bd37d6b0c1081be0b5f7cf5356b
Jake Mackie <jake.wmackie@gmail.com>
2025-11-21 15:30:58 +0000

    feat: tailwind setup

--- src/theme/vert/functions.php
+++ src/theme/vert/functions.php
@@ -1,10 +1,6 @@
 <?php
 
-// Temporary
-function vert_custom_enqueue_styles() {
-    wp_enqueue_style( 'vert-custom-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version') );
-}
-add_action( 'wp_enqueue_scripts', 'vert_custom_enqueue_styles' );
-
+// Enqueue styles and scripts
+require_once get_template_directory() . '/includes/enqueue-styles.php';
 // Custom post types
 require_once get_template_directory() . '/includes/custom-post-types.php';
